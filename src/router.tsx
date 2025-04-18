ea2ccd31ce5cc946af5af58eda78e190c7d75519
pushkar1713 <pushkar1713@gmail.com>
2025-04-18 20:11:52 +0530

    final commit 2

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Placeholder from "./pages/Placeholder";
 import RegistrationForm from "./components/Registration";
 import Login from "./pages/Login";
 
@@ -9,7 +8,6 @@ export const appRouter = createBrowserRouter([
     path: "/",
     element: <App />, // Wrap everything inside App
     children: [
-
       { path: "/register", element: <RegistrationForm /> },
       { path: "/", element: <Login /> },
     ],

a14e96dc286c8c0b1ade762c17040ae3638a3344
pushkar1713 <pushkar1713@gmail.com>
2025-05-16 01:00:21 +0530

    auth flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,37 @@
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const REDIRECT_URL = import.meta.env.VITE_REDIRECT_URL;
+
+const checkAuth = async () => {
+  try {
+    const res = await axios.get(`${BACKEND_URL}/api/v1/investor/checkAuth`, {
+      withCredentials: true,
+    });
+    return res.data.authenticated;
+  } catch {
+    console.log("Not authenticated");
+  }
+};
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const verifyAuthenticationAndRedirect = async () => {
+      const authenticated = await checkAuth();
+      if (authenticated) {
+        window.location.href = REDIRECT_URL;
+      } else {
+        navigate("/");
+      }
+    };
+
+    verifyAuthenticationAndRedirect();
+  }, []);
   return (
     <>
       <Outlet />
